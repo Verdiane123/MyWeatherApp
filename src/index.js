a895352ebd96002a2c1bce2048f8980799dd8af4
Verdiane123 <verdiane123@gmail.com>
2024-05-27 23:04:30 +0200

    Updated images and weather details

--- src/index.js
+++ src/index.js
@@ -4,17 +4,33 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+
+  let detailsElement = document.querySelector("#details");
+  detailsElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  humidityElement.innerHTML = `${humidity} %`;
+
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  windElement.innerHTML = `${wind} km/h`;
+
+  let iconElement = document.querySelector("#icon");
+  let icon = response.data.condition.icon_url;
+  iconElement.innerHTML = `<img src="${icon}" alt="image">`;
 }
 
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-
+function search(city) {
   let apiKey = "4dffe8c4c23fa008oed1a0td3c39336b";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  search(searchInputElement.value);
 }
 
 function formatDate(date) {
@@ -37,21 +53,18 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
 
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
 
-
-
-
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearchSubmit);
+search("London");
